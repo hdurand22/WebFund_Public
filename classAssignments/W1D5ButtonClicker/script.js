bb03170582366e31d6156efca6a24d23c59453b6
Hayden Durand <75761674+hdurand22@users.noreply.github.com>
2021-06-07 13:54:38 -0700

    Delete classAssignments directory

--- classAssignments/W1D5ButtonClicker/script.js
+++
@@ -1,8 +0,0 @@
-var partyCounter = 0; // For counting clicks
-
-function gifOn () {
-    var gif = document.getElementById("surprise");
-    gif.style.display = "inherit";
-    partyCounter++;
-    console.log("Party times: " + partyCounter);
-}
